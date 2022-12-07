2ef17f1ec29806bdb40a53b832ed1ad2bcd779f8
kraigoris <109440395+kraigoris@users.noreply.github.com>
2022-12-07 12:33:32 +0300

    Refactor day 6

--- apps/day_06/lib/day06.ex
+++ apps/day_06/lib/day06.ex
@@ -1,49 +1,43 @@
 defmodule Day06 do
   def part1() do
-    get_input()
-    |> String.trim()
-    |> String.graphemes()
-    |> Enum.reduce_while({[], 1}, fn char, {window, current_index} ->
-      new_window = slide4(window, char)
-
-      if is_packet_marker?(new_window) do
-        {:halt, {new_window, current_index}}
-      else
-        {:cont, {new_window, current_index + 1}}
-      end
-    end)
+    get_characters()
+    |> traverse_char_list(4)
     |> elem(1)
   end
 
   def part2() do
-    get_input()
-    |> String.trim()
-    |> String.graphemes()
-    |> Enum.reduce_while({[], 1}, fn char, {window, current_index} ->
-      new_window = slide14(window, char)
+    get_characters()
+    |> traverse_char_list(14)
+    |> elem(1)
+  end
 
-      if is_message_marker?(new_window) do
+  def traverse_char_list(lst, marker_size) do
+    Enum.reduce_while(lst, {[], 1}, fn char, {window, current_index} ->
+      new_window = slide(window, char, marker_size)
+
+      if is_marker?(new_window, marker_size) do
         {:halt, {new_window, current_index}}
       else
         {:cont, {new_window, current_index + 1}}
       end
     end)
-    |> elem(1)
   end
 
-  def is_message_marker?(lst) when length(lst) === 14, do: Enum.count(Enum.uniq(lst)) === 14
-  def is_message_marker?(_lst), do: false
+  def is_marker?(lst, size), do: Enum.count(Enum.uniq(lst)) === size
 
-  def is_packet_marker?(lst) when length(lst) === 4, do: Enum.count(Enum.uniq(lst)) === 4
-  def is_packet_marker?(_lst), do: false
+  def slide(lst, new_elem, window_size) do
+    case lst do
+      [] -> [new_elem]
+      [_ | rest] when length(lst) === window_size -> rest ++ [new_elem]
+      lst -> lst ++ [new_elem]
+    end
+  end
 
-  def slide14([], new_elem), do: [new_elem]
-  def slide14([_ | rest] = lst, new_elem) when length(lst) == 14, do: rest ++ [new_elem]
-  def slide14(lst, new_elem), do: lst ++ [new_elem]
-
-  def slide4([], new_elem), do: [new_elem]
-  def slide4([_ | rest] = lst, new_elem) when length(lst) == 4, do: rest ++ [new_elem]
-  def slide4(lst, new_elem), do: lst ++ [new_elem]
+  def get_characters() do
+    get_input()
+    |> String.trim()
+    |> String.graphemes()
+  end
 
   def get_input() do
     __ENV__.file
